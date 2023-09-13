0ac33d5099e5a5c9baa3a8a08c27334856fc4e44
n.mgebrishvili <n.mgebrishvili@paybox.ge>
2023-09-13 14:59:33 +0400

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {createElement as e} from 'react';
 
 
 function App() {
-  return(
-      <h1>Hello react</h1>
-  )
+    // return(
+    //     <h1>Hello aaa</h1>
+    // )
+    return e('h1', {className: 'container'}, [
+        e('h1', {className: 'font-bold'}, "text KSX"),
+        e('button', {className: '',}, 'click me')
+    ])
 }
 
 export default App;
